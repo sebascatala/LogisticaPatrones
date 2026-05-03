98cd8af90cfa26d2463da4a1ec63709ec2bb81ad
Esteban Pardo <presteban949@gmail.com>
2026-05-02 22:42:38 -0400

    metodo de envio Director

--- Events/ChainOfResponsability/Director.cs
+++ Events/ChainOfResponsability/Director.cs
@@ -1,5 +1,22 @@
 public class Director:ManejadorIncidencia
 {
+    public void generarOrden(string tipoCarga)
+    {
+        switch(tipoCarga.ToLower())
+        {
+            case"estandar":
+            creador = new CreadorEstandar();
+            break;
+            case"refrigerado":
+            creador = new CreadorRefrigerado();
+            break;
+            case"valor declarado":
+            creador = new CreadorValorDeclarado();
+            break;
+            default:
+            throw new Exception("Tipo de envio no soportado");
+        }
+    }
     public override void Manejar(Incidencia incidencia)
     {
         if(Incidencia.Nivel <=3)
